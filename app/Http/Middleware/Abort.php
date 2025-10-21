c53ebde2a16388400042b7dff284ff9c5ef99872
Milele <milelesergeevna2001@gmail.com>
2025-10-21 11:14:19 +0500

    Add middleware

---
+++ app/Http/Middleware/Abort.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Abort
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response =$next($request);
+        if ($response->getStatusCode() == 404){
+        return response()->json([
+        "message" => "Not found",
+        "code" => 404
+        ], 404);
+    }
+    return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::fallback( function () {
+//     return response()->json([
+//         "message" => "Not found",
+//         "code" => 404
+//     ], 404);
+// });
