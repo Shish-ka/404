3fc4a69d119c5048fcb4986780bf84ae4853bc53
Milele <milelesergeevna2001@gmail.com>
2025-10-21 11:22:31 +0500

    Add fallback

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/', function () {
+    return 'Hello api';
+});
+Route::fallback(function () {
+    return response()->json([
+        "message"=> "Not found",
+        "code"=> 404
+    ], 404);
+});
