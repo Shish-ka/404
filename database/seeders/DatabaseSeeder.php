e8f590d95392c6e6a779c31e06177c119f3f9090
Milele <milelesergeevna2001@gmail.com>
2025-10-24 21:13:50 -0700

    update user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,13 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Test Admin',
+            'email' => 'admin@example.com',
+            'password' => 1111,
+            'is_admin' => true,
         ]);
+
+        User::factory(10)->create();
     }
 }
