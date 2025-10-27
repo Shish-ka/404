8d53768f4e184f7a7108f6ec94832c3f62ad86fe
Shish-ka <milenaandreevna2007@gmail.com>
2025-10-27 09:41:41 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +9,10 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 Route::get('/', function () {
     return response()->json('Hello api');
+});
+
+Route::controller(UserController::class)->group(function() {
+    Route::get('users', 'index');
+    Route::get('users/{user}', 'show');
 });
 
